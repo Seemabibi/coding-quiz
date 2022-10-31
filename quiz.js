dc343f75328bb18dd71bb8dddb0f1d97fcfed89c
seemabibi <seema.bibi@hotmail.co.uk>
2022-10-31 17:10:22 +0000

    added var elements to js

---
+++ quiz.js
@@ -0,0 +1,6 @@
+var StartQuiz = document.getElementById("StartQuiz")
+var SaveScore = document.getElementById("SaveScore")
+var ViewScore = document.getElementById("ViewScore")
+var PlayAgain = document.getElementById("PlayAgain")
+
+va
