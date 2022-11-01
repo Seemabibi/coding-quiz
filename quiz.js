05d9156a59c13a5445655c130919d9d92245f407
seemabibi <seema.bibi@hotmail.co.uk>
2022-11-01 18:46:45 +0000

    changed html errors

--- quiz.js
+++ quiz.js
@@ -1,18 +1,18 @@
 var StartQuiz = document.getElementById("StartQuiz")
 var SaveScore = document.getElementById("SaveScore")
-var ViewScore = document.getElementById("ViewScore")
+var ViewScore = document.getElementById("ViewScores")
 var PlayAgain = document.getElementById("PlayAgain")
 
 var Welcome = document.getElementById("Welcome")
 var Quiz = document.getElementById("Quiz")
-var Result = document.getElementById("Result")
+var Result = document.getElementById("end")
 
 var Options = document.getElementById("Options")
 var Message = document.getElementById("Message")
 
 var Timer = document.getElementById("Timer")
 
-var Summary = document.getElementById("Summary")
+var Summary = document.getElementById("summary")
 var SecondsLeft = 0;
 var Score = 0;
 var CurrentQuestion = 0;
@@ -20,8 +20,8 @@ var CountdownTimer;
 
 function StopGame() {
     // Stop the countdown timer 
-    ClearInterval(CountdownTimer)
-   
+    clearInterval(CountdownTimer)
+
     // clear the timer 
     Timer.textContent = ""
 
@@ -32,6 +32,7 @@ function StopGame() {
     // Display the score
     Summary.textContent = "Your Score Is: " + score;
 }
+
 function onSaveScore(e) {
     var initals = document.getElementById("initials").value
     // If we have valid initials, save the score to local storage 
@@ -40,11 +41,13 @@ function onSaveScore(e) {
         document.getElementById("initials").value = "";
     }
 }
+
 function onViewScores(e) {
     Window.location.href = 'scores.html'
 }
+
 function onSelectAnswer(e) {
-    var CorrectAnswer = questions[currentQuestions].answer;
+    var CorrectAnswer = questions[CurrentQuestion].answer;
     var UserAnswer = e.target.textContent;
     if (CorrectAnswer == UserAnswer) {
         score++;
@@ -56,22 +59,24 @@ function onSelectAnswer(e) {
     // Call up the next question
     displayQuestion();
 }
+
 function displayMessage(msg) {
     // Display the Message
     Message.textContent = msg;
 
     // Clear the message afer 1 second
     setTimeout(function () {
-        message.textContent = "";
+        Message.textContent = "";
     }, 1000);
 }
+
 function displayQuestion() {
     // Increment to get the next question
     currentQuestion++;
     console.log('Current Question Is ' + currentQuestion);
 
     // Have we ran out of questions?
-    if (currentQuestion >= question.length) {
+    if (currentQuestion >= questions.length) {
         StopGame();
         return;
     }
@@ -85,13 +90,15 @@ function displayQuestion() {
     // Load through the choice and output the new possible options 
     for (var i = 0; i < question.choices.length; i++) {
         var option = document.createElement("div");
-        option.textContent = question.choices[1];
-        option.oneclick = onSelectAnswer;
+        option.textContent = question.choices[i];
+        option.onclick = onSelectAnswer;
         option.classList.add("option");
         Options.appendChild(option);
     }
 }
+
 function onStartGame() {
+
     // Set the timer at 70 seconds 
     secondsLeft = 70;
     // Start at the first question 
@@ -107,11 +114,14 @@ function onStartGame() {
         }
         secondsLeft--;
     }, 1000);
-    document.getElementById('greeting').style.display = "none";
-    end.style.display = "none"; codeQuiz.style.display = "flex";
+
+    document.getElementById('Welcome').style.display = "none";
+    end.style.display = "none";
+    Quiz.style.display = "flex";
     displayQuestion();
 }
-beginquiz.adEventListener("click", onStartGame);
+
+StartQuiz.addEventListener("click", onStartGame);
 SaveScore.addEventListener("click", onSaveScore);
-ViewScore.addEventListener("click", OnViewScore);
-PlayAgain.addEventListener("click", OnStartGame);
+ViewScore.addEventListener("click", onViewScores);
+PlayAgain.addEventListener("click", onStartGame);
